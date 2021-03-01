76b4791723c76f852e1ec0ba9ff7f5756a6e014a
Arshad <arshadakjsa@gmail.com>
2021-03-01 09:44:14 +0530

    python file containing function performed in main.py

---
+++ functions.py
@@ -0,0 +1,34 @@
+def conversion(unit1, unit2, value):
+    if unit1 == unit2:
+        return value
+
+    if unit1 == 'sq ft':
+        if unit2 == 'sq m':
+            return value / 10.7639
+        elif unit2 == 'cent':
+            return value / 435.599
+        elif unit2 == 'ares':
+            return value / 1076.39
+
+    elif unit1 == 'sq m':
+        if unit2 == 'sq ft':
+            return value * 10.7639
+        elif unit2 == 'cent':
+            return value / 40.46856
+        elif unit2 == 'ares':
+            return value / 100
+
+    elif unit1 == 'sq cent':
+        if unit2 == 'sq m':
+            return value * 40.46856
+        elif unit2 == 'sq ft':
+            return value * 435.599
+        elif unit2 == 'ares':
+            return value / 2.47067
+    else:
+        if unit2 == 'sq m':
+            return value * 100
+        elif unit2 == 'sq ft':
+            return value * 1076.39
+        elif unit2 == 'cent':
+            return value * 2.47067
