cbdb6327fc7a6e8c10ab00c43520a09e5804615e
Arshad <arshadakjsa@gmail.com>
2021-03-06 14:05:30 +0530

    added some more methods

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ from kivymd.uix.menu import MDDropdownMenu
 from kivymd.uix.button import MDFlatButton
 
 
+class WindowManager(ScreenManager):
+    pass
+
+
+# declaring both screen
 class AreaWindow(Screen):
     # function to calculate area of triangle with three sides given
     area_total = 0  # initializing total area to zero
@@ -21,39 +26,54 @@ class AreaWindow(Screen):
             c1 = float(self.c.text)  # side c
             s = (a1 + b1 + c1) / 2
             ar = math.sqrt(s * (s - a1) * (s - b1) * (s - c1))  # Heron's Formulae
+            self.result_last.text = 'ar(ABC) = ' + str(round(ar, 3)) + ' m\u00b2'
             AreaWindow.area_total += ar
             AreaWindow.area_total = round(AreaWindow.area_total, 3)
-            self.result.text = str(AreaWindow.area_total) + ' m\u00b2'
-        # when an invalid input is entered a pop up will appear
+            self.result_total.text = 'Total = ' + str(AreaWindow.area_total) + ' m\u00b2'
+
         except Exception:
+            # when an invalid input is entered a pop up will appear
             close = MDFlatButton(text="Close", on_release=self.dialog_close)
-            self.dialog = MDDialog(title="Value Error", text="Invalid Input", size_hint=(0.7, 1), buttons=[close])
+            self.dialog = MDDialog(
+                title="Value Error",
+                text="Invalid Input",
+                size_hint=(0.7, 1),
+                buttons=[close]
+            )
             self.dialog.open()
 
     def dialog_close(self, obj):
         # when 'Close' button in dialog box pressed, it will call this function
         self.dialog.dismiss()
 
+    def clear_all(self):
+        no = MDFlatButton(text="NO", on_release=self.dialog_close)
+        yes = MDFlatButton(text="YES", on_release=self.total_area_dismiss)
+        self.dialog = MDDialog(
+            title="Warning",
+            text="Are you sure?",
+            size_hint=(0.7, 1),
+            buttons=[no, yes]
+        )
+        self.dialog.open()
+
+    def total_area_dismiss(self, obj):
+        # to clear the total area
+        self.dialog.dismiss()
+        AreaWindow.area_total = 0
+        self.result_total.text = ''
+        self.result_last.text = ''
+
 
 class ConversionWindow(Screen):
     pass
 
 
-class WindowManager(ScreenManager):
-    pass
-
-
 class MainApp(MDApp):
+    Window.size = (380, 680)
     # main app
-    # the ids present in the .kv files
-    a = ObjectProperty(None)
-    b = ObjectProperty(None)
-    c = ObjectProperty(None)
-    result = ObjectProperty(None)
-    window = ObjectProperty(None)
-    # initializing variables used in the app
-    dropdown1 = None
-    dropdown2 = None
+    window = ObjectProperty(None)  # id of WindowManager
+    # initializing both units to None
     unit1 = None
     unit2 = None
 
@@ -63,14 +83,21 @@ class MainApp(MDApp):
 
         # items in the dropdown list of conversion screen
         item = [{'text': i, "callback": self.callback1} for i in ('sq ft', 'sq m', 'cent', 'ares')]
+
         # dropdown menu of the 'From' button in the conversion screen
-        self.dropdown1 = MDDropdownMenu(width_mult=3,
-                                        caller=self.build_app.window.get_screen('conversion').ids.drop1,
-                                        items=item, callback=self.callback1)
+        self.dropdown1 = MDDropdownMenu(
+            width_mult=2.8,
+            caller=self.build_app.window.get_screen('conversion').ids.drop1,
+            items=item,
+            callback=self.callback1
+        )
         # dropdown menu of the 'To' button in the conversion screen
-        self.dropdown2 = MDDropdownMenu(width_mult=3,
-                                        caller=self.build_app.window.get_screen('conversion').ids.drop2,
-                                        items=item, callback=self.callback2)
+        self.dropdown2 = MDDropdownMenu(
+            width_mult=2.8,
+            caller=self.build_app.window.get_screen('conversion').ids.drop2,
+            items=item,
+            callback=self.callback2
+        )
 
     def callback1(self, unit):
         # function called when a list item of the 'From' button is pressed
@@ -82,14 +109,23 @@ class MainApp(MDApp):
         # function called when a list item of the 'To' button is pressed
         self.root.window.get_screen('conversion').ids.drop2.text = unit.text  # the text in the button is changed
         self.unit2 = unit.text  # the text in the selected menu item is saved
-        self.dropdown1.dismiss()  # closes the dropdown menu after a menu item is selected
+        self.dropdown2.dismiss()  # closes the dropdown menu after a menu item is selected
 
-    def conv(self):
+    def unit_conversion(self):
 
         # function called when 'CONVERT' button is pressed
         if self.unit1 is None or self.unit2 is None:  # when no units are chosen, dialog box will appear
-            close = MDFlatButton(text="Close", on_release=self.dialog_close)  # close button for the dialog box
-            self.dialog = MDDialog(title="Unit Error", text="Select units", size_hint=(0.7, 1), buttons=[close])
+            # close button for the dialog box
+            close = MDFlatButton(
+                text="Close",
+                on_release=self.dialog_close
+            )
+            self.dialog = MDDialog(
+                title="Unit Error",
+                text="Select units",
+                size_hint=(0.7, 1),
+                buttons=[close]
+            )
             self.dialog.open()
 
         else:
@@ -97,13 +133,21 @@ class MainApp(MDApp):
                 # conversion from one unit to the other
                 value = float(self.root.window.get_screen('conversion').ids.unity.text)
                 # conversion function from the functions module
-                res = round(functions.conversion(self.unit1, self.unit2, value), 3)
-                self.root.window.get_screen('conversion').ids.convert.text = f'{str(res)} {self.unit2}'
+                conversion_result = round(functions.conversion(self.unit1, self.unit2, value), 3)
+                self.root.window.get_screen('conversion').ids.convert.text = f'{str(conversion_result)} {self.unit2}'
 
             except Exception:
                 # when the input value is invalid(not a number) dialog box will appear
-                close = MDFlatButton(text="Close", on_release=self.dialog_close)
-                self.dialog = MDDialog(title="Value Error", text="Invalid Input", size_hint=(0.7, 1), buttons=[close])
+                close = MDFlatButton(
+                    text="Close",
+                    on_release=self.dialog_close
+                )
+                self.dialog = MDDialog(
+                    title="Value Error",
+                    text="Invalid Input",
+                    size_hint=(0.7, 1),
+                    buttons=[close]
+                )
                 self.dialog.open()
 
     def dialog_close(self, obj):
@@ -111,17 +155,22 @@ class MainApp(MDApp):
         self.dialog.dismiss()
 
     def build(self):
-        Window.size = (360, 600)
         return self.build_app  # returning the loaded .kv file
 
-    def to_convert(self, ins):
-        # function to change the screen the floating action button is pressed
+    def to_convert(self):
+        # function to change the screen when the floating action button is pressed
         self.root.current = 'conversion'
+        self.root.transition.direction = "left"
+        self.root.window.get_screen('areawindow').ids.stack_button.close_stack()
 
-    def to_area(self, ins):
-        # function to change the screen the floating action button is pressed
+    def to_area(self):
+        # function to change the screen when the floating action button is pressed
         self.root.current = 'areawindow'
         self.root.transition.direction = "right"
+        self.unit1 = None
+        self.unit2 = None
+        self.root.window.get_screen('conversion').ids.drop1.text = 'From'
+        self.root.window.get_screen('conversion').ids.drop2.text = 'To'
 
 
 if __name__ == '__main__':
